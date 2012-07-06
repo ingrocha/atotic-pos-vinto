66d66f30045691541f5d9df43fc2688a40ddb1fd
miranda.adriana2010@gmail.com <miranda.adriana2010@gmail.com@2196e273-b58a-fec0-35b0-e4500e454758>
2012-07-06 14:58:07 +0000

    manejo de usuarios y asignacion de sucursales

--- trunk/posvinto/app/Controller/UsuariosController.php
+++ trunk/posvinto/app/Controller/UsuariosController.php
@@ -1,224 +1,96 @@
 <?php
-App::uses('AppController', 'Controller');
-/**
- * Usuarios Controller
- *
- * @property Usuario $Usuario
- * @property AclComponent $Acl
- * @property SessionComponent $Session
- */
-class UsuariosController extends AppController {
+class UsuariosController extends AppController
+{ 
 
-/**
- * Helpers
- *
- * @var array
- */
-	public $helpers = array('Ajax', 'Javascript', 'Html', 'Form');
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Acl', 'Session');
-    
-    public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow('*');
-    }
+    public $helpers = array('Html', 'Form'); 
+    public $uses = array('Usuario', 'Perfile','Departamento', 'Sucursal', 'Estado'); 
+    public function index()
+    {
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->Usuario->recursive = 0;
-		$this->set('usuarios', $this->paginate());
-	}
-
-/**
- * view method
- *
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		$this->Usuario->id = $id;
-		if (!$this->Usuario->exists()) {
-			throw new NotFoundException(__('Invalid usuario'));
-		}
-		$this->set('usuario', $this->Usuario->read(null, $id));
-	}
-
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Usuario->create();
-			if ($this->Usuario->save($this->request->data)) {
-				$this->Session->setFlash(__('The usuario has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The usuario could not be saved. Please, try again.'));
-			}
-		}
-		$perfiles = $this->Usuario->Perfile->find('list');
-		$this->set(compact('perfiles'));
-	}
-
-/**
- * edit method
- *
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		$this->Usuario->id = $id;
-		if (!$this->Usuario->exists()) {
-			throw new NotFoundException(__('Invalid usuario'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Usuario->save($this->request->data)) {
-				$this->Session->setFlash(__('The usuario has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The usuario could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Usuario->read(null, $id);
-		}
-		$perfiles = $this->Usuario->Perfile->find('list');
-		$this->set(compact('perfiles'));
-	}
-
-/**
- * delete method
- *
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Usuario->id = $id;
-		if (!$this->Usuario->exists()) {
-			throw new NotFoundException(__('Invalid usuario'));
-		}
-		if ($this->Usuario->delete()) {
-			$this->Session->setFlash(__('Usuario deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Usuario was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
-/**
- * admin_index method
- *
- * @return void
- */
-	public function admin_index() {
-		$this->Usuario->recursive = 0;
-		$this->set('usuarios', $this->paginate());
-	}        
-
-/**
- * admin_view method
- *
- * @param string $id
- * @return void
- */
-	public function admin_view($id = null) {
-		$this->Usuario->id = $id;
-		if (!$this->Usuario->exists()) {
-			throw new NotFoundException(__('Invalid usuario'));
-		}
-		$this->set('usuario', $this->Usuario->read(null, $id));
-	}
-
-/**
- * admin_add method
- *
- * @return void
- */
-	public function admin_add() {
-		if ($this->request->is('post')) {
-			$this->Usuario->create();
-			if ($this->Usuario->save($this->request->data)) {
-				$this->Session->setFlash(__('The usuario has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The usuario could not be saved. Please, try again.'));
-			}
-		}
-		$perfiles = $this->Usuario->Perfile->find('list');
-		$this->set(compact('perfiles'));
-	}
-
-/**
- * admin_edit method
- *
- * @param string $id
- * @return void
- */
-	public function admin_edit($id = null) {
-		$this->Usuario->id = $id;
-		if (!$this->Usuario->exists()) {
-			throw new NotFoundException(__('Invalid usuario'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Usuario->save($this->request->data)) {
-				$this->Session->setFlash(__('The usuario has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The usuario could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Usuario->read(null, $id);
-		}
-		$perfiles = $this->Usuario->Perfile->find('list');
-		$this->set(compact('perfiles'));
-	}
-
-/**
- * admin_delete method
- *
- * @param string $id
- * @return void
- */
-	public function admin_delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Usuario->id = $id;
-		if (!$this->Usuario->exists()) {
-			throw new NotFoundException(__('Invalid usuario'));
-		}
-		if ($this->Usuario->delete()) {
-			$this->Session->setFlash(__('Usuario deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Usuario was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
-    
-    public function login(){
+        $usuarios = $this->Usuario->find('all'); 
+        $this->set(compact('usuarios'));
         
-        if ($this->request->is('post')) {
-            
-            if ($this->Auth->login()) {
-                $this->redirect($this->Auth->redirect());
-            } else {
-                $this->Session->setFlash('Your username or password was incorrect.');
-            }
-        }    
     }
-    
-    public function logout(){
-        $this->redirect($this->Auth->logout());
+
+    public function nuevo()
+    {
+        
+        if (!empty($this->data)) { 
+
+            $this->Usuario->create(); 
+            $pass = sha1($this->data['Usuario']['pass']);
+            $this->request->data['Usuario']['pass']= $pass;
+            if ($this->Usuario->save($this->data)) { 
+
+                $this->Session->setFlash('Usuario registrado con exito'); 
+                $this->redirect(array('action' => 'index'), null, true); 
+
+            } else { 
+
+                $this->Session->setFlash('No se pudo registrar el Usuario'); 
+            }
+            
+        }
+         $dperf = $this->Perfile->find('list', array('fields'=>'Perfile.nombre'));
+        $sucursales = $this->Sucursal->find('list', array('fields'=>array('Sucursal.id', 'Sucursal.nombre')));
+        $estados = $this->Estado->find('list', array('fields'=>array('Estado.nombre')));
+       $this->set(compact('dperf', 'sucursales', 'estados'));
+  
+        
+    }
+
+    public function modificar($id = null)
+    {
+        $this->Usuario->id = $id;
+        if (!$id) {
+            $this->Session->setFlash('No existe tal registro');
+            $this->redirect(array('action' => 'index'), null, true);
+        }
+        if (empty($this->data)) {
+            $this->data = $this->Usuario->read();
+
+        } else {
+            if ($this->Usuario->save($this->data)) {
+                $this->Session->setFlash('Los datos fueron modificados');
+                $this->redirect(array('action' => 'index'), null, true);
+            } else {
+                $this->Session->setFlash('no se pudo modificar!!');
+            }
+        }
+        $dperf = $this->Perfile->find('list', array('fields'=>'Perfile.nombre'));
+       // debug($dperf);
+        $sucursales = $this->Sucursal->find('list', array('fields'=>array('Sucursal.id', 'Sucursal.nombre')));
+        $estados = $this->Estado->find('list', array('fields'=>array('Estado.nombre')));
+       $this->set(compact('dperf', 'sucursales', 'estados'));
+        
+    }
+
+    public function baja($id = null)
+    {
+        if (!$id) {
+            $this->Session->setFlash('id Invalida para borrar');
+            $this->redirect(array('action' => 'index'));
+        }
+        $baja = $this->Estado->find('first', array('conditions'=>array('Estado.nombre LIKE'=>"baja")));
+        $this->Usuario->id = $id;
+        $this->request->data['Usuario']['estado_id'] = $baja['Estado']['id'];
+        if ($this->Usuario->save($this->data)) {
+            
+            $this->Session->setFlash('El usuario  ' . $id . ' fue dado de baja');
+            $this->redirect(array('action' => 'index'));
+        }
+    }
+    public function eliminar($id = null)
+    {
+        if (!$id) {
+            $this->Session->setFlash('id Invalida para borrar');
+            $this->redirect(array('action' => 'index'));
+        }
+        
+        if ($this->Usuario->delete($id)) {
+            $this->Session->setFlash('El usuario  ' . $id . ' fue eliminado');
+            $this->redirect(array('action' => 'index'));
+        }
     }
 }
+?>
