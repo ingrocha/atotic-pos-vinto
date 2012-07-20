f7e3eb98c1d0a39cd1a1dc70f0ca8e0eb553f004
miranda.adriana2010 <miranda.adriana2010@gmail.com>
2012-07-20 16:31:09 +0000

    mis reporeste de filtros

--- posvinto/app/Controller/ReportesController.php
+++ posvinto/app/Controller/ReportesController.php
@@ -1,7 +1,7 @@
 <?php 
 class ReportesController extends AppController{
     public $helpers = array('Html', 'Form'); 
-    public $uses = array('Producto', 'Movimientosinsumo', 'Pedido', 'Item');
+    public $uses = array('Producto', 'Movimientosinsumo', 'Pedido', 'Item', 'Usuario');
     public $components = array('Session');
     public $layout = 'admin';
     
@@ -20,7 +20,7 @@ class ReportesController extends AppController{
     $this->set(compact('pedidos'));
     }
     public function detallepedido($id=null, $mozo=null){
-        //$this->layout = 'imprimir';
+        $this->layout = 'imprimir';
         $items = $this->Item->find(
            'all',
            array(
@@ -31,19 +31,207 @@ class ReportesController extends AppController{
         $this->set(compact('items', 'mozo'));        
     }
     public function mozos(){
+        if(!empty($this->data)){
+          
+           $fecha = $this->data['Producto']['fecha']." %";
+           $mozo = $this->data['Producto']['mozos'];
+           
+           $pedidos = $this->Pedido->find(
+           'all', array(
+           'conditions'=>array('Pedido.usuario_id'=>$mozo, 'Pedido.fecha like'=>$fecha), 
+           'order'=>array('Pedido.id asc')
+           )
+           );
+           
+           $mozo = $pedidos[0]['Usuario']['nombre'];
+           $this->set(compact('pedidos', 'mozo'));   
+        }else{
+            $mozos = $this->Usuario->find('list', array('conditions'=>array('Usuario.perfile_id'=>2), 'fields'=>array('Usuario.nombre')));
+            $this->set(compact('mozos'));
+        }
         
     }
     public function insumos(){
         
     }
+    public function buscar(){
+        $data=$this->data['dato'];
+        $options=array(
+        'OR'=>array(
+        'Pedido.mesa LIKE' =>'%'.$data.'%',
+        'Usuario.nombre LIKE' =>'%'.$data.'%',
+        'Pedido.fecha LIKE' =>'%'.$data.'%',
+        'Pedido.total LIKE' =>'%'.$data.'%'
+        )
+        );
+        /*$result=$this->Pedido->find('all',array('conditions'=>array($options)));
+        debug($result);exit;*/
+        $this->paginate  = array(
+            'conditions'=>array($options),
+            'limit' => 25,        
+            'order' => array(
+            'Pedido.id' => 'desc'
+            )
+            );
+
+    // similar to findAll(), but fetches paged results
+       $data = $this->paginate('Pedido');
+
+       $mozos = $this->Usuario->find('list', array('conditions'=>array('Usuario.perfile_id'=>2), 'fields'=>array('Usuario.nombre')));
+       $this->set(compact('data', 'mozos'));
+
+    }
+    function buscar2(){
+           // debug($this->data);
+            $data= $this->data['dato'];
+            $data1 = $this->data['estado'];
+            $tipo = $this->data['tipo'];
+            
+                if($this->data['estado'] != null ){
+                    if($data != null){
+                     $options=array( 'Usuario.estado' =>$data1,
+                                        'OR'=>array(
+                                             
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.usuario LIKE' =>'%'.$data.'%',
+                                            'Usuario.ci LIKE' =>'%'.$data.'%',
+                                            'Usuario.correo LIKE' =>'%'.$data.'%'
+                                            )
+                                        );
+                             }else{
+                                  $options=array('Usuario.estado' =>$data1);
+                                           
+                             }
+            
+                     }elseif($tipo != null){
+                         if($data != null){
+                             $options=array( 'Tipousuario.id' =>$tipo,
+                                        'OR'=>array(
+                                            
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.usuario LIKE' =>'%'.$data.'%',
+                                            'Usuario.ci LIKE' =>'%'.$data.'%',
+                                            'Usuario.correo LIKE' =>'%'.$data.'%'
+                                            )
+                                        );
+                         }else{
+                             $options=array(
+                                       
+                                           'Tipousuario.id' =>$tipo);
+                                            
+                         }
+                          
+                     }
+                     else{
+                         $options=array(
+                                        'OR'=>array(
+                                             'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.usuario LIKE' =>'%'.$data.'%',
+                                            'Usuario.ci LIKE' =>'%'.$data.'%',
+                                            'Usuario.correo LIKE' =>'%'.$data.'%'
+                                            )
+                                        );
+                     }
+    
+   
+   
+       $tipos = $this->Tipousuario->find('list',array('fields'=>array('Tipousuario.tipo')));
+      
+      
+    $result=$this->Usuario->find('all',array('conditions'=>array($options)));
+ // debug($result);exit;
+     $this->set('tipos', $tipos);
+     $this->set('all',$result);
+    }
     public function pedidos(){
-        $fecha = date('Y-m-d')." %";
-        
-        $pedidoshoy = $this->Pedido->find('all', array(
+        if(!empty($this->data)){
+            debug($this->data);exit;
+            $dato = "";
+            $fecha = "";
+            $mozo = "";
+            
+             if($this->data['estado'] != null ){
+                    if($data != null){
+                     $options=array( 'Usuario.estado' =>$data1,
+                                        'OR'=>array(
+                                             
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.usuario LIKE' =>'%'.$data.'%',
+                                            'Usuario.ci LIKE' =>'%'.$data.'%',
+                                            'Usuario.correo LIKE' =>'%'.$data.'%'
+                                            )
+                                        );
+                             }else{
+                                  $options=array('Usuario.estado' =>$data1);
+                                           
+                             }
+            
+                     }elseif($tipo != null){
+                         if($data != null){
+                             $options=array( 'Tipousuario.id' =>$tipo,
+                                        'OR'=>array(
+                                            
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.usuario LIKE' =>'%'.$data.'%',
+                                            'Usuario.ci LIKE' =>'%'.$data.'%',
+                                            'Usuario.correo LIKE' =>'%'.$data.'%'
+                                            )
+                                        );
+                         }else{
+                             $options=array(
+                                       
+                                           'Tipousuario.id' =>$tipo);
+                                            
+                         }
+                          
+                     }
+                     else{
+                         $options=array(
+                                        'OR'=>array(
+                                             'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.nombre LIKE' =>'%'.$data.'%',
+                                            'Usuario.usuario LIKE' =>'%'.$data.'%',
+                                            'Usuario.ci LIKE' =>'%'.$data.'%',
+                                            'Usuario.correo LIKE' =>'%'.$data.'%'
+                                            )
+                                        );
+                     }
+            
+            
+            
+            $this->paginate  = array(
+            'conditions'=>array($options),
+            'limit' => 25,        
+            'order' => array(
+            'Pedido.id' => 'desc'
+            )
+            );
+
+    // similar to findAll(), but fetches paged results
+            $data = $this->paginate('Pedido');
+    
+            
+       /* $pedidos = $this->Pedido->find('all', array(
         'conditions'=>array('Pedido.fecha like'=>$fecha)
-        ));
-     
-        $this->set(compact('pedidoshoy'));
+        ));*/
+        }else{
+            $this->paginate  = array(
+            'limit' => 25,
+            'order' => array(
+            'Pedido.id' => 'desc'
+            )
+            );
+            
+            // similar to findAll(), but fetches paged results
+            $data = $this->paginate('Pedido');
+        }
+        $mozos = $this->Usuario->find('list', array('conditions'=>array('Usuario.perfile_id'=>2), 'fields'=>array('Usuario.nombre')));
+        $this->set(compact('data', 'mozos'));
     }
     
 }
